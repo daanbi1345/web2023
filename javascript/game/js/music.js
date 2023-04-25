b919046dd551d6d7310cfbbd22895f75c49dc4aa
daanbi1345 <daanbi1345@gmail.com>
2023-04-25 19:01:34 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,56 @@
+const allMusic = [
+    {
+        name : "1. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "2. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "3. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "4. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "5. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "6. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "7. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "8. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "9. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "10. 음악제목",
+        artist : "가수 이름",
+        img : "music_view01",
+        audio: "music_audio01"
+    }
+    
+
+    
+]
